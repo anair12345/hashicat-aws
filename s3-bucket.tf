04d3cb13cc2e79503e933520e0760bbc0e06ed45
Akhil Nair <anair54321@gmail.com>
2021-10-07 12:48:06 +0100

    create s3-bucket.tf

---
+++ s3-bucket.tf
@@ -0,0 +1,13 @@
+module "s3_bucket" {
+  source  = "terraform-aws-modules/s3-bucket/aws"
+  version = "2.2.0"
+
+  bucket = "my-s3-bucket"
+  acl    = "private"
+  bucket_prefix = "akhilnair"
+
+  versioning = {
+    enabled = true
+  }
+
+}
